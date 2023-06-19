b5715a00f861ae84ab8cbf6bef037093f17ac3eb
luka1104 <skyward.luke11@gmail.com>
2023-06-20 08:57:45 +0900

    Add Swiper and fetch keys in Key page

--- nhouse-mock/src/pages/key.tsx
+++ nhouse-mock/src/pages/key.tsx
@@ -1,8 +1,39 @@
 import { NextPage } from "next"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination } from "swiper"
+import "swiper/css"
+import "swiper/css/pagination"
+import axios from "axios"
+import PropertyReservationCard from "@/components/PropertyReservationCard"
+import { usePrivy } from "@privy-io/react-auth"
 
 const Key: NextPage = () => {
-  return <div>Key</div>
+  const [keys, setKeys] = useState([])
+  const { user } = usePrivy()
+
+  const fetchKeys = async () => {
+    if (!user?.wallet?.address) return
+    const res = await axios.post("/api/fetchKeys", { address: user?.wallet?.address })
+    if (!res.data) return
+    console.log(res.data)
+    setKeys(res.data)
+  }
+
+  useEffect(() => {
+    fetchKeys()
+  }, [user])
+  return (
+    <>
+      <Swiper modules={[Pagination]} spaceBetween={50} slidesPerView={1} pagination={{ clickable: true }}>
+        {keys.map((property) => (
+          <SwiperSlide>
+            <PropertyReservationCard property={property} onOpen={onOpen} isLoading={isLoading} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  )
 }
 
 export default Key
