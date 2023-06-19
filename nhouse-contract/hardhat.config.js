1d7a2888145783da6f307fc5bd1195de5f714be6
luka1104 <skyward.luke11@gmail.com>
2023-06-20 04:25:18 +0900

    Change default network to "astar" in config

--- nhouse-contract/hardhat.config.js
+++ nhouse-contract/hardhat.config.js
@@ -7,10 +7,10 @@ require("@nomiclabs/hardhat-ethers")
 const { API_URL, PRIVATE_KEY } = process.env
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "goerli",
+  defaultNetwork: "astar",
   networks: {
     hardhat: {},
-    goerli: {
+    astar: {
       url: `${API_URL}`,
       accounts: [`0x${PRIVATE_KEY}`],
     },
