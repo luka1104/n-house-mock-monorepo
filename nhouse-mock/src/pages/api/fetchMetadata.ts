27c120be02803b2daae1ad93fa69c2820260e81a
luka1104 <skyward.luke11@gmail.com>
2023-06-20 05:44:12 +0900

    Add web3 to dependencies and fetchMetadata.ts

--- nhouse-mock/src/pages/api/fetchMetadata.ts
+++ nhouse-mock/src/pages/api/fetchMetadata.ts
@@ -1,4 +1,9 @@
 import { NextRequest, NextResponse } from "next/server"
+import Web3 from "web3"
+const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const CONTRACT_ADDRESS = "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
+const contract = require("../artifacts/contracts/NhouseNFT.sol/NhouseNFT.json")
 
 const handler = async (req: NextRequest, res: NextResponse) => {
   const provider = new Web3(new Web3.providers.HttpProvider(RPC))
