869f3853c6d9e27c9d726337431473d49cd856fc
luka1104 <skyward.luke11@gmail.com>
2023-06-20 05:13:41 +0900

    Update web3 version and refactor fetchMetadata script

--- nhouse-contract/scripts/fetchMetadata.js
+++ nhouse-contract/scripts/fetchMetadata.js
@@ -1,20 +1,14 @@
 require("dotenv").config()
+const Web3 = require("web3")
 const API_URL = process.env.API_URL
 const PUBLIC_KEY = process.env.PUBLIC_KEY
-const contractAddress = "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
+const CONTRACT_ADDRESS = "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
 const contract = require("../artifacts/contracts/NhouseNFT.sol/NhouseNFT.json")
 
 const fetchMetadata = async () => {
-  const provider = new Web3(new Web3.providers.HttpProvider(API_URL))
-  const Contract = new provider.eth.Contract(contract.abi, contractAddress, { from: PUBLIC_KEY })
-  await Contract.methods.getTokenUriFromAddress(req.body.address).call((err, tokenUris) => {
-    if (err) {
-      console.log("An error occured", err)
-      return
-    }
-    tokenUris = tokenUris.map((uri) => JSON.parse(uri))
-    console.log(tokenUris)
-  })
+  const web3 = new Web3(API_URL)
+  const nftContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS)
+  nftContract.methods.getTokenUriFromAddress(PUBLIC_KEY).call().then(console.log)
 }
 
 fetchMetadata()
