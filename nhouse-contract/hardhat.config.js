aa2de39756e005b7037ef5eb69000b0fa14d242d
luka1104 <skyward.luke11@gmail.com>
2023-06-20 03:52:04 +0900

    Update Solidity version to 0.8.9 and add hardhat-toolbox

--- nhouse-contract/hardhat.config.js
+++ nhouse-contract/hardhat.config.js
@@ -1,21 +1,22 @@
 /**
  * @type import(‘hardhat/config’).HardhatUserConfig
  */
+require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-ethers")
 const { API_URL, GOERLI_RPC, PRIVATE_KEY } = process.env
 module.exports = {
-  solidity: "0.8.4",
-  defaultNetwork: "mumbai",
-  networks: {
-    hardhat: {},
-    goerli: {
-      url: `${GOERLI_RPC}`,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
-    mumbai: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
-  },
+  solidity: "0.8.9",
+  // defaultNetwork: "mumbai",
+  // networks: {
+  //   hardhat: {},
+  //   goerli: {
+  //     url: `${GOERLI_RPC}`,
+  //     accounts: [`0x${PRIVATE_KEY}`],
+  //   },
+  //   mumbai: {
+  //     url: API_URL,
+  //     accounts: [`0x${PRIVATE_KEY}`],
+  //   },
+  // },
 }
