5a0664277a98c70207c40713f341112b55fa2c84
luka1104 <skyward.luke11@gmail.com>
2023-06-20 06:09:18 +0900

    Add NEXT_PUBLIC env variables for NFT metadata API

--- nhouse-mock/src/pages/api/fetchMetadata.ts
+++ nhouse-mock/src/pages/api/fetchMetadata.ts
@@ -1,9 +1,10 @@
 import Web3 from "web3"
 import contract from "@/contracts/NhouseNFT.json"
 import { NextApiRequest, NextApiResponse } from "next"
-const API_URL = process.env.API_URL || "https://evm.astar.network"
-const PUBLIC_KEY = process.env.PUBLIC_KEY || "0x872449c44937f6Ac266cbBCDCb189B25AcEBb9E9"
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://evm.astar.network"
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY || "0x872449c44937f6Ac266cbBCDCb189B25AcEBb9E9"
+const CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const web3 = new Web3(API_URL)
