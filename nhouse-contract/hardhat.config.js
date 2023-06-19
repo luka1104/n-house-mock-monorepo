8063a6c0f5702acb66bbbafd7ee6c8533bc0a843
luka1104 <skyward.luke11@gmail.com>
2023-06-20 03:47:25 +0900

    Update hardhat configuration for Mumbai network

--- nhouse-contract/hardhat.config.js
+++ nhouse-contract/hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @type import(‘hardhat/config’).HardhatUserConfig
+ */
+require("dotenv").config()
+require("@nomiclabs/hardhat-ethers")
+const { API_URL, GOERLI_RPC, PRIVATE_KEY } = process.env
 module.exports = {
-  solidity: "0.8.18",
-};
+  solidity: "0.8.4",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `${GOERLI_RPC}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+}
