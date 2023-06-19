a90aa4d9cbb5016753a2f2549b49b8c482147406
luka1104 <skyward.luke11@gmail.com>
2023-06-20 05:53:41 +0900

    Refactor fetchMetadata API handler

--- nhouse-mock/src/pages/api/fetchMetadata.ts
+++ nhouse-mock/src/pages/api/fetchMetadata.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from "next/server"
 import Web3 from "web3"
-const API_URL = process.env.API_URL
-const PUBLIC_KEY = process.env.PUBLIC_KEY
-const CONTRACT_ADDRESS = "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
-const contract = require("../artifacts/contracts/NhouseNFT.sol/NhouseNFT.json")
+import contract from "@/contracts/NhouseNFT.json"
+import { NextApiRequest, NextApiResponse } from "next"
+const API_URL = process.env.API_URL || "https://evm.astar.network"
+const PUBLIC_KEY = process.env.PUBLIC_KEY || "0x"
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || "0xfEE418A3c546580c5fe6D57113179951bfef62e5"
 
-const handler = async (req: NextRequest, res: NextResponse) => {
-  const provider = new Web3(new Web3.providers.HttpProvider(RPC))
-  const Contract = new provider.eth.Contract(contract.abi, contractAddress, { from: PUBLIC_KEY })
-  await Contract.methods.getTokenUriFromAddress(req.body.address).call((err, tokenUris) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const web3 = new Web3(API_URL)
+  const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
+  await nftContract.methods.getTokenUriFromAddress(PUBLIC_KEY).call((err: any, tokenUris: any) => {
     if (err) {
       console.log("An error occured", err)
       res.status(500).send("An error occured")
       return
     }
-    tokenUris = tokenUris.map((uri) => JSON.parse(uri))
+    tokenUris = tokenUris.map((uri: string) => JSON.parse(uri))
     console.log(tokenUris)
-    res.status(200).send(tokenUris)
+    res.status(200).json(tokenUris)
   })
 }
