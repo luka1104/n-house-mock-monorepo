6d5471f29a55bbe9d5ccb3b5172b77719fbe1c82
luka1104 <skyward.luke11@gmail.com>
2023-06-20 08:04:08 +0900

    Add error handling for sign and transfer errors

--- nhouse-mock/src/pages/api/handleReserve.ts
+++ nhouse-mock/src/pages/api/handleReserve.ts
@@ -7,7 +7,7 @@ const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY || ""
 const CONTRACT_ADDRESS =
   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "0xeeF566179E2896aFE3bA1E456088e6fe670C4801"
 
-const handleTransfer = async (web3: any, address: string, tokenId: string) => {
+const handleTransfer = async (web3: any, address: string, tokenId: string, res: NextApiResponse) => {
   const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
   const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest")
   const data = await nftContract.methods.transferFrom(PUBLIC_KEY, address, JSON.parse(tokenId)).encodeABI()
@@ -19,10 +19,10 @@ const handleTransfer = async (web3: any, address: string, tokenId: string) => {
     from: address,
   }
   const resp = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, async (err: any, signedTx: any) => {
-    if (err) return console.log("SIGN ERROR", err)
+    if (err) return res.status(500).json({ error: err })
     console.log("SIGNING", signedTx)
     await web3.eth.sendSignedTransaction(signedTx.rawTransaction as string, (err: any, resp: any) => {
-      if (err) return console.log("TRANSFER ERROR", err)
+      if (err) return res.status(500).json({ error: err })
       console.log("RESERVING", resp)
     })
   })
@@ -32,7 +32,7 @@ const handleTransfer = async (web3: any, address: string, tokenId: string) => {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { address, tokenId } = req.body
   const web3 = new Web3(API_URL)
-  const resp = await handleTransfer(web3, address, tokenId)
+  const resp = await handleTransfer(web3, address, tokenId, res)
   console.log(resp)
   const interval = setInterval(function () {
     console.log("Attempting to get transaction receipt...")
