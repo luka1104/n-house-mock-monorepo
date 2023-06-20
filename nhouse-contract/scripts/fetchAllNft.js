0757888874c81d1d1cca10c13e3129c922aa32b8
luka1104 <skyward.luke11@gmail.com>
2023-06-20 20:13:24 +0900

    create test script

---
+++ nhouse-contract/scripts/fetchAllNft.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const Web3 = require("web3")
+const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const CONTRACT_ADDRESS = "0x6Cc552e296dBfedE18DE507d19c067EaC4037c7b"
+const contract = require("../artifacts/contracts/NhouseNFT.sol/NhouseNFT.json")
+
+const fetchAll = async () => {
+  const web3 = new Web3(API_URL)
+  const nftContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS)
+  nftContract.methods.getAllTokens().call().then(console.log)
+}
+
+fetchAll()
