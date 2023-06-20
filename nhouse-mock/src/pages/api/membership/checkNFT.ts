392dbdededd7774c543248a207d82c91d978733e
luka1104 <skyward.luke11@gmail.com>
2023-06-20 22:08:08 +0900

    Check NFT ownership for given address

--- nhouse-mock/src/pages/api/membership/checkNFT.ts
+++ nhouse-mock/src/pages/api/membership/checkNFT.ts
@@ -7,21 +7,22 @@ const CONTRACT_ADDRESS =
   process.env.NEXT_PUBLIC_MEMBERSHIP_ADDRESS || "0x7B4a600b78fC6534B4125145cd38e45d366ebD28"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const address = req.body.address
   const web3 = new Web3(API_URL)
   const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
-  await nftContract.methods.getTokenUriFromAddress(PUBLIC_KEY).call((err: any, tokenUris: any) => {
+  await nftContract.methods.getTokenUriFromAddress(address).call((err: any, tokenUris: any) => {
     if (err) {
       console.log("An error occured", err)
       res.status(500).send("An error occured")
       return
     }
-    const formattedUris = tokenUris.map((tokenUri: any) => {
-      return {
-        tokenId: tokenUri[0],
-        tokenUri: JSON.parse(tokenUri[1]),
-      }
-    })
-    console.log(formattedUris)
-    res.status(200).json(formattedUris)
+    console.log(tokenUris)
+    if (tokenUris.length != 0) {
+      res.status(200).json(true)
+      return
+    } else {
+      res.status(200).json(false)
+      return
+    }
   })
 }
