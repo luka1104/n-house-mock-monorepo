fee3588dad7c518b14952cbd9d1892680d1c590f
luka1104 <skyward.luke11@gmail.com>
2023-06-20 13:01:22 +0900

    Refactor code formatting and quotes

--- nhouse-mock/src/lib/utils.ts
+++ nhouse-mock/src/lib/utils.ts
@@ -1,20 +1,14 @@
-import { clsx, type ClassValue } from 'clsx'
-import { customAlphabet } from 'nanoid'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from "clsx"
+import { customAlphabet } from "nanoid"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  7
-) // 7-character random string
+export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 7) // 7-character random string
 
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<JSON> {
+export async function fetcher<JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
   const res = await fetch(input, init)
 
   if (!res.ok) {
@@ -26,7 +20,7 @@ export async function fetcher<JSON = any>(
       error.status = res.status
       throw error
     } else {
-      throw new Error('An unexpected error occurred')
+      throw new Error("An unexpected error occurred")
     }
   }
 
@@ -35,9 +29,9 @@ export async function fetcher<JSON = any>(
 
 export function formatDate(input: string | number | Date): string {
   const date = new Date(input)
-  return date.toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
   })
 }
