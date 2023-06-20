6c95a4b1b028887d017cf38c68d2ade9ecf11309
luka1104 <skyward.luke11@gmail.com>
2023-06-20 21:20:23 +0900

    update batchmint

--- nhouse-mock/src/pages/api/batchMint.ts
+++ nhouse-mock/src/pages/api/batchMint.ts
@@ -7,12 +7,12 @@ const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY || ""
 const CONTRACT_ADDRESS =
   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "0x83979FB1FD5664cB59784Aa86f1bBaaf5DdFf7c0"
 
-const handleMint = async (web3: any, metadata: string) => {
+const handleBatchMint = async (web3: any, tokenUris: any[], res: NextApiResponse) => {
   const nftContract = new web3.eth.Contract((contract as any).abi, CONTRACT_ADDRESS)
   const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest")
-  const data = await nftContract.methods.mintNFT(PUBLIC_KEY, metadata).encodeABI()
+  const data = await nftContract.methods.batchMintNFT(PUBLIC_KEY, tokenUris).encodeABI()
   const tx = {
-    gas: 500000,
+    gas: 5000000,
     to: CONTRACT_ADDRESS,
     nonce: nonce,
     data: data,
@@ -20,14 +20,13 @@ const handleMint = async (web3: any, metadata: string) => {
   }
   const resp = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY, async (err: any, signedTx: any) => {
     if (err) {
-      console.log(err)
-
+      res.status(500).json({ error: err })
       return
     }
     console.log("SIGNING", signedTx)
     await web3.eth.sendSignedTransaction(signedTx.rawTransaction as string, (err: any, resp: any) => {
       if (err) {
-        console.log(err)
+        res.status(500).json({ error: err })
         return
       }
       console.log("RESERVING", resp)
@@ -40,41 +39,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { dates } = req.body
   const web3 = new Web3(API_URL)
 
-  // await dates.map(async (date: string) => {
-  //   setTimeout(async () => {
-  //     const metadata = JSON.stringify({
-  //       name: "Nhouse NFT",
-  //       description: "An NFT from Nhouse",
-  //       image: "https://art.pixilart.com/82d984fcd46cafb.gif",
-  //       propertyName: "Nhouse Blue",
-  //       reservedDate: date,
-  //     })
+  let tokenUris: any[] = []
+  dates.map((date: any) => {
+    tokenUris.push({
+      name: "Nhouse NFT",
+      description: "An NFT from Nhouse",
+      image: "https://art.pixilart.com/82d984fcd46cafb.gif",
+      propertyName: "Nhouse Blue",
+      reservedDate: date,
+    })
+  })
 
-  //     const resp = await handleMint(web3, metadata)
-  //     console.log(resp)
-  //   }, 500)
-  // })
-  const delay = (ms: any) => new Promise((res) => setTimeout(res, ms))
+  const resp = await handleBatchMint(web3, tokenUris, res)
 
-  async function processDates(dates: any) {
-    for (let date of dates) {
-      const metadata = JSON.stringify({
-        name: "Nhouse NFT",
-        description: "An NFT from Nhouse",
-        image: "https://art.pixilart.com/82d984fcd46cafb.gif",
-        propertyName: "Nhouse Blue",
-        reservedDate: date,
+  if (resp.hash) {
+    const interval = setInterval(function () {
+      console.log("Attempting to get transaction receipt...")
+      web3.eth.getTransactionReceipt(resp.hash, function (err, rec) {
+        if (rec) {
+          console.log(rec)
+          clearInterval(interval)
+          res.status(200).json({ receipt: rec })
+        }
+        if (err) {
+          console.log(err)
+          clearInterval(interval)
+          res.status(500).json({ error: err })
+        }
       })
-
-      const resp = await handleMint(web3, metadata)
-      console.log(resp)
-
-      await delay(1000)
-    }
-    return true
+    }, 1000)
   }
-
-  const resp = await processDates(dates)
-
-  if (resp) res.status(200).json({ status: "ok" })
 }
