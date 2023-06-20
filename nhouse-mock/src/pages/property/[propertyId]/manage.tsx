1766fd742760dbc11fab922ca0e6e2fce84d7e3d
luka1104 <skyward.luke11@gmail.com>
2023-06-20 19:33:20 +0900

    Add property validation in Manage page

--- nhouse-mock/src/pages/property/[propertyId]/manage.tsx
+++ nhouse-mock/src/pages/property/[propertyId]/manage.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from "next"
 import React from "react"
+import { useRouter } from "next/router"
+import { properties } from "@/data/mockdata"
 
 const Manage: NextPage = () => {
+  const router = useRouter()
+  const propertyId = router.query.propertyId
+  const property = properties.find((property) => property.id === propertyId)
+  if (!property) {
+    return <div>loading...</div>
+  }
   return <div>Manage</div>
 }
 
