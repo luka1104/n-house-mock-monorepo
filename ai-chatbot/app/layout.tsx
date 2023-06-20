ab48898f4ba426e4bc622ecc89e736cf9b6ae97d
luka1104 <skyward.luke11@gmail.com>
2023-06-20 15:43:11 +0900

    Remove PrivyProvider from layout component

--- ai-chatbot/app/layout.tsx
+++ ai-chatbot/app/layout.tsx
@@ -9,7 +9,6 @@ import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import Header from '@/components/header'
 import { ChakraProvider } from '@chakra-ui/react'
-import { PrivyProvider } from '@privy-io/react-auth'
 
 export const metadata: Metadata = {
   title: {
@@ -43,33 +42,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
           fontMono.variable
         )}
       >
-        <PrivyProvider
-          createPrivyWalletOnLogin
-          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
-          config={{
-            appearance: {
-              // This configures your login modal to show wallet login options above other options.
-              showWalletLoginFirst: false
-            },
-            // This configures wallet, email, Google, and Twitter login for your app.
-            loginMethods: ['wallet', 'email', 'google', 'apple']
-          }}
-          onSuccess={() => window.location.reload()}
-        >
-          <Toaster />
-          <Providers attribute="class" defaultTheme="system" enableSystem>
-            <ChakraProvider>
-              <div className="flex min-h-screen flex-col">
-                {/* @ts-ignore */}
-                <Header />
-                <main className="bg-muted/50 flex flex-1 flex-col">
-                  {children}
-                </main>
-              </div>
-            </ChakraProvider>
-            <TailwindIndicator />
-          </Providers>
-        </PrivyProvider>
+        <Toaster />
+        <Providers attribute="class" defaultTheme="system" enableSystem>
+          <ChakraProvider>
+            <div className="flex min-h-screen flex-col">
+              {/* @ts-ignore */}
+              <Header />
+              <main className="bg-muted/50 flex flex-1 flex-col">
+                {children}
+              </main>
+            </div>
+          </ChakraProvider>
+          <TailwindIndicator />
+        </Providers>
       </body>
     </html>
   )
